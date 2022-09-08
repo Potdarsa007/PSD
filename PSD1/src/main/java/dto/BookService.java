911185263347b164868df74878f340b82b5cb374
D5_62797_SwapnilPotdar <potdarswapnil007@gmail.com>
2022-09-08 12:37:05 +0530

    BookService dto added

--- PSD1/src/main/java/dto/BookService.java
+++ PSD1/src/main/java/dto/BookService.java
@@ -11,22 +11,28 @@ public class BookService {
 
 	private String userEmail;
 	private String professionName;
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	public String getProfessionName() {
-		return professionName;
-	}
-	public void setProfessionName(String professionName) {
-		this.professionName = professionName;
-	}
+	
 	public BookService(String userEmail, String professionName) {
 		super();
 		this.userEmail = userEmail;
 		this.professionName = professionName;
 	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getProfessionName() {
+		return professionName;
+	}
+
+	public void setProfessionName(String professionName) {
+		this.professionName = professionName;
+	}
+	
 	
 }
